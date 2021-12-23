c293e4cd4998bb2f7e9563698c98a310fa56a166
CecileBlau <92298805+CecileBlau@users.noreply.github.com>
2021-12-23 10:39:13 +0200

    HACKATON FINAL

---
+++ script.js
@@ -0,0 +1,14 @@
+//when the submit button is clicked:
+//retrieve the buttonLocation
+document.getElementById("submitButton")
+
+//add an event listener to the buttonLocation
+//buttonLocation.addEventListener("submit", submitFunction)
+
+//create the submitFunction
+//function submitForm(e){
+//e.preventDefault();
+
+
+
+//}
